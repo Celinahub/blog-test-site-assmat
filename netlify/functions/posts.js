bf8b6a1e61f72d559ec3cac4b14b59e224f9bd4e
Celinahub <poupoune80@yahoo.com>
2024-09-19 16:13:34 +0200

    gestion articles4

--- netlify/functions/posts.js
+++ netlify/functions/posts.js
@@ -1,22 +1,19 @@
+// netlify/functions/posts.js
+
 const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 
 exports.handler = async (event) => {
   try {
-    // Répertoire où se trouvent les fichiers Markdown
     const postsDir = path.join(process.cwd(), '_posts');
     const files = fs.readdirSync(postsDir);
 
-    // Lire et traiter chaque fichier Markdown
     const posts = files.map(file => {
       const filePath = path.join(postsDir, file);
       const content = fs.readFileSync(filePath, 'utf-8');
       
-      // Séparer les métadonnées du corps du contenu
       const [metadata, body] = content.split('---').filter(Boolean);
-
-      // Convertir les métadonnées en un objet
       const metadataLines = metadata.split('\n');
       const postData = {};
       metadataLines.forEach(line => {
@@ -26,14 +23,12 @@ exports.handler = async (event) => {
         }
       });
 
-      // Convertir le corps Markdown en HTML
       return {
         ...postData,
         body: marked(body.trim())
       };
     });
 
-    // Répondre avec les articles au format JSON
     return {
       statusCode: 200,
       body: JSON.stringify(posts),
